d24321310e7e0e78ca6f8b3a7ea8c6be630ab819
Ben McCallum <ben.mccallum@isobar.com>
2015-03-20 17:17:31 +1100

    Added some more extensions for list control to try and select a item of value safely.

--- CoreWeb/Sys/Web/UI/WebControls/ListControlExtensions.cs
+++ CoreWeb/Sys/Web/UI/WebControls/ListControlExtensions.cs
@@ -13,4 +13,30 @@ public static class ListControlExtensions
     {
         return String.IsNullOrEmpty(listControl.SelectedValue) ? defaultValue : listControl.SelectedValue;
     }
+
+    /// <summary>
+    /// Select an item safely. If an item with the given <paramref name="value"/> doesn't exist, just don't select anything.
+    /// </summary>
+    /// <param name="listControl">ListControl</param>
+    /// <param name="value">Value to try an select.</param>
+    public static void SelectItemSafely(this ListControl listControl, string value)
+    {
+        listControl.TrySelectItem(value);
+    }
+
+    /// <summary>
+    /// Select an item safely. If an item with the given <paramref name="value"/> doesn't exist, just don't select anything.
+    /// </summary>
+    /// <param name="listControl">ListControl</param>
+    /// <param name="value">Value to try an select.</param>
+    /// <returns>Bool indicating whether an item was found and selected.</returns>
+    public static bool TrySelectItem(this ListControl listControl, string value)
+    {
+        if (listControl.Items.FindByValue(value) != null)
+        {
+            listControl.SelectedValue = value;
+            return true;
+        }
+        return false;
+    }
 }
