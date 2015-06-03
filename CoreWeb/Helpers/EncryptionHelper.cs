46891bb3702f681eb42f22f7e7473e44bf798af2
Ben McCallum <ben.mccallum@live.com.au>
2015-06-03 15:18:07 +1000

    Update to add an EncryptionHelper that compliments the CoreSystem encryption helper by adding support for encoding to allow DOM and URL usage.

---
+++ CoreWeb/Helpers/EncryptionHelper.cs
@@ -0,0 +1,23 @@
+﻿using CoreSystem.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CoreWeb.Helpers
+{
+    public static class EncryptionHelper
+    {
+        public static string EncryptAndEncode(string input)
+        {
+            return HttpServerUtility.UrlTokenEncode(TripleDESHelper.EncryptBytes(Encoding.UTF8.GetBytes(input)));
+        }
+
+        public static string DecryptAfterDecode(string input)
+        {
+            return Encoding.UTF8.GetString(TripleDESHelper.DecryptBytes(HttpServerUtility.UrlTokenDecode(input)));
+        }
+    }
+}
